3fd1e5c7dafdeba451996c2fc04a55c4ff52a3a6
Thomas Nägele <tom@naegele.it>
2019-09-24 11:17:31 +0200

    - implement last updated date

--- .eleventy.js
+++ .eleventy.js
@@ -2,3 +2,18 @@ module.exports = function(eleventyConfig) {
   // Copy the `_img/` directory
   eleventyConfig.addPassthroughCopy("_assets/");
 };
+
+const moment = require('moment');
+ 
+moment.locale('de');
+
+module.exports = function (eleventyConfig) {
+
+  eleventyConfig.addFilter('dateIso', date => {
+    return moment(date).toISOString();
+  });
+
+  eleventyConfig.addFilter('dateReadable', date => {
+    return moment(date).format('LL'); // E.g. May 31, 2019
+  });
+};
